861117a584cb6bf983123ba8feab89956b25e62e
NARARA30 <nara.makne0@gmail.com>
2026-01-06 15:42:56 +0900

    db 정보 수정

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ import mysql from 'mysql2/promise';
 export const db = await mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '1234',
+  password: '1cpmaina!',
   port: 3306,
-  database: 'elice', // 실제 DB명으로 바꿔
+  database: 'narara10', // 실제 DB명으로 바꿔
   waitForConnections: true,
   connectionLimit: 10,
 });
